c1cd660f716681ebfb2edf7bf3d9edfa0b966b60
sophiemassola <sophiemassola.sm@gmail.com>
2022-02-26 20:50:05 -0300

    Versión corregida

--- entregable.js
+++ entregable.js
@@ -5,7 +5,7 @@ let palabra = ''
 
 for (let i =0; i<3; i=i+1){
     palabra = prompt(`¡Tienes ${3-i} intentos para adivinar la palabra y obtener un 5% de descuento en tu compra!: S _ _ _ _ E `)
-    
+    .toUpperCase(palabra);
     if (i<=2 && palabra=='SUERTE'){
         alert('¡CORRECTO! Has ganado un 5% de descuento en tu próxima compra con el código COMPRA5%')
         break;
@@ -20,19 +20,21 @@ let descuento = prompt('Ingrese su código de descuento. Si no tiene, presione E
 let precio = parseInt(prompt('Indique el precio del producto que desea comprar:'));
 
 
+
 // Funciones ↓↓
-const doce = x => x*1.15;
+/*const doce = x => x*1.15;
 const seis = x => x*1.1;
 const docecuotas = x => x*1.15/12;
-const seiscuotas = x => x*1.1/6;
+const seiscuotas = x => x*1.1/6;*/
 
+const intereses = (a,b) => a*b;
 
 // Condicionales ↓↓
 if (precio >= 7500 & descuento != 'COMPRA5%'){
-    let interesseis = seis(precio);
-    let cuotasseis = seiscuotas (precio);
-    let interesdoce = doce(precio);
-    let cuotasdoce = docecuotas (precio);
+    let interesseis = intereses(precio,1.1);
+    let cuotasseis = interesseis/6;
+    let interesdoce = intereses(precio,1.15);
+    let cuotasdoce = interesdoce/12;
     alert(`
     El precio del producto es elegible para 6 y 12 cuotas con intereses. 
     
@@ -41,10 +43,10 @@ if (precio >= 7500 & descuento != 'COMPRA5%'){
     6 cuotas de $${cuotasdoce}.  Precio final: ${interesdoce}.`)
 }   
     else if (precio >= 7500 & descuento == 'COMPRA5%'){
-        let interesseis = seis(precio)*0.95;
-        let cuotasseis = seiscuotas (precio)*0.95;
-        let interesdoce = doce(precio)*0.95;
-        let cuotasdoce = docecuotas (precio)*0.95;
+        let interesseis = intereses(precio,1.1)*0.95;
+    let cuotasseis = interesseis/6;
+    let interesdoce = intereses(precio,1.15)*0.95;
+    let cuotasdoce = interesdoce/12;
         alert(`
         El precio del producto es elegible para 6 y 12 cuotas con intereses. 
         
